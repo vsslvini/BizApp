161dc98431ff3d305816301cedcede02e91eb0c7
Vinicius Alencar <vsslviniciussousa@gmail.com>
2025-09-22 10:19:47 -0400

    Correções e melhorias em inconsistencias no DB

--- src/storage/initializeDb.ts
+++ src/storage/initializeDb.ts
@@ -4,45 +4,48 @@ import { type SQLiteDatabase } from "expo-sqlite"
 export async function InitializeDb(database: SQLiteDatabase) {
   await database.execAsync(`
         CREATE TABLE IF NOT EXISTS produtos (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,  
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
           nome TEXT NOT NULL,
-          precoCusto FLOAT NOT NULL,
-          precoVenda FLOAT NOT NULL,
+          precoCusto REAL NOT NULL,
+          precoVenda REAL NOT NULL,
           quantidadeEstoque INTEGER NOT NULL
         );
+          
         CREATE TABLE IF NOT EXISTS clientes (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
           nome TEXT NOT NULL,
           email TEXT,
-          telefone TEXT,
-          dividas TEXT
+          telefone TEXT
         );
-        CREATE TABLE IF NOT EXISTS venda (
+          
+        CREATE TABLE IF NOT EXISTS vendas (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
           dataVenda TEXT NOT NULL,
-          valorTOTAL REAL NOT NULL,
+          valorTotal REAL NOT NULL,
           tipoPagamento TEXT NOT NULL,
           clienteId INTEGER,
           FOREIGN KEY (clienteId) REFERENCES clientes(id)
         );
+          
         CREATE TABLE IF NOT EXISTS itensVenda (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
           vendaId INTEGER NOT NULL,
           produtoId INTEGER NOT NULL,
-          uantidade INTEGER NOT NULL,
+          quantidade INTEGER NOT NULL,
           precoUnitarioVenda REAL NOT NULL,
-          FOREIGN KEY (vendaId) REFERENCES venda(id),
+          FOREIGN KEY (vendaId) REFERENCES vendas(id),
           FOREIGN KEY (produtoId) REFERENCES produtos(id)
         );
+          
         CREATE TABLE IF NOT EXISTS dividasPendencias (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
           clienteId INTEGER NOT NULL,
+          vendaId INTEGER,
           valor REAL NOT NULL,
           dataRegistro TEXT NOT NULL,
           dataPagamento TEXT,
           status INTEGER NOT NULL DEFAULT 0,
           descricao TEXT,
-          vendaId INTEGER,
           FOREIGN KEY (clienteId) REFERENCES clientes(id),
           FOREIGN KEY (vendaId) REFERENCES vendas(id)
         );
